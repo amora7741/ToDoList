7f4682c25ccad08bdf7a0082a21a9fb5c85eddd3
amora7741 <mora7741@gmail.com>
2024-01-24 14:27:07 -0800

    Tab spacing

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,82 +1,82 @@
-import renderHeader from "../components/header.js";
-import renderTaskListCard from "../components/tasklistcard.js";
-import Project from "../components/project.js";
-import createTaskList from "../components/tasklist.js";
-import renderModal from "../components/modal.js";
+import renderHeader from '../components/header.js';
+import renderTaskListCard from '../components/tasklistcard.js';
+import Project from '../components/project.js';
+import createTaskList from '../components/tasklist.js';
+import renderModal from '../components/modal.js';
 
 export default function renderMain(container) {
-	function updateTaskList() {
-		const taskListCard = document.querySelector(".tasklist");
-		if (taskListCard) {
-			taskListCard.remove();
-		}
+  function updateTaskList() {
+    const taskListCard = document.querySelector('.tasklist');
+    if (taskListCard) {
+      taskListCard.remove();
+    }
 
-		taskCard.appendChild(
-			createTaskList(currentProject, () =>
-				saveData(projects, currentProjectIndex)
-			)
-		);
-	}
+    taskCard.appendChild(
+      createTaskList(currentProject, () =>
+        saveData(projects, currentProjectIndex)
+      )
+    );
+  }
 
-	const projectsData = JSON.parse(localStorage.getItem("projects")) || [];
-	let currentProjectIndex = localStorage.getItem("currentProjectIndex") || 0;
+  const projectsData = JSON.parse(localStorage.getItem('projects')) || [];
+  let currentProjectIndex = localStorage.getItem('currentProjectIndex') || 0;
 
-	let projects = [];
+  let projects = [];
 
-	projectsData.forEach((projectData) => {
-		const project = new Project(projectData.name);
-		project.tasks = projectData.tasks;
-		projects.push(project);
-	});
+  projectsData.forEach((projectData) => {
+    const project = new Project(projectData.name);
+    project.tasks = projectData.tasks;
+    projects.push(project);
+  });
 
-	let currentProject;
+  let currentProject;
 
-	if (projects.length === 0) {
-		currentProject = new Project("Default Project");
-		projects.push(currentProject);
-	} else {
-		currentProject = projects[currentProjectIndex];
-	}
+  if (projects.length === 0) {
+    currentProject = new Project('Default Project');
+    projects.push(currentProject);
+  } else {
+    currentProject = projects[currentProjectIndex];
+  }
 
-	container.appendChild(
-		renderHeader(
-			projects,
-			currentProjectIndex,
-			(newIndex) => {
-				currentProjectIndex = newIndex;
-				currentProject = projects[currentProjectIndex];
-				updateTaskList();
-			},
-			(projectName) => {
-				const newProject = new Project(projectName);
-				projects.push(newProject);
-				currentProjectIndex = projects.length - 1;
-				currentProject = newProject;
-				saveData(projects, currentProjectIndex);
-				updateTaskList();
-			}
-		)
-	);
-	container.appendChild(
-		renderTaskListCard(currentProject, () =>
-			saveData(projects, currentProjectIndex)
-		)
-	);
+  container.appendChild(
+    renderHeader(
+      projects,
+      currentProjectIndex,
+      (newIndex) => {
+        currentProjectIndex = newIndex;
+        currentProject = projects[currentProjectIndex];
+        updateTaskList();
+      },
+      (projectName) => {
+        const newProject = new Project(projectName);
+        projects.push(newProject);
+        currentProjectIndex = projects.length - 1;
+        currentProject = newProject;
+        saveData(projects, currentProjectIndex);
+        updateTaskList();
+      }
+    )
+  );
+  container.appendChild(
+    renderTaskListCard(currentProject, () =>
+      saveData(projects, currentProjectIndex)
+    )
+  );
 
-	const taskCard = document.querySelector("main");
+  const taskCard = document.querySelector('main');
 
-	document.getElementById("addtask").addEventListener("click", () => {
-		container.appendChild(
-			renderModal((newTodo) => {
-				currentProject.addTodo(newTodo);
-				updateTaskList();
-				saveData(projects, currentProjectIndex);
-			})
-		);
-	});
+  document.getElementById('addtask').addEventListener('click', () => {
+    container.appendChild(
+      renderModal((newTodo) => {
+        currentProject.addTodo(newTodo);
+        updateTaskList();
+        saveData(projects, currentProjectIndex);
+      })
+    );
+  });
 }
 
 function saveData(projects, currentProjectIndex) {
-	localStorage.setItem("projects", JSON.stringify(projects));
-	localStorage.setItem("currentProjectIndex", currentProjectIndex);
+  localStorage.setItem('projects', JSON.stringify(projects));
+  localStorage.setItem('currentProjectIndex', currentProjectIndex);
 }
